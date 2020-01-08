110a58791463d851f4b7cfecde7846e4cc4cd6ec
Jiri Kucera <jkucera@redhat.com>
2020-01-08 01:10:10 +0100

    .travis/preinstall: Add python3-selinux
    Add python3-selinux to the packages-to-be-installed list if
    venv python matches system python.

---
+++ .travis/config.sh
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: MIT
+#
+# Use this file to specify custom configuration for a project. Generally, this
+# involves the modification of the content of LSR_* environment variables, see
+#
+#   * .travis/preinstall:
+#
+#       - LSR_EXTRA_PACKAGES
+#
